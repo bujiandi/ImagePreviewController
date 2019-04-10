1a3f6189598dd6a983cb806485797e6b92e09c1f
李招利 <Swift@lizhaolideiMac.local>
2019-04-10 14:31:26 +0800

    v0.1.0

---
+++ ImagePreviewController/ImagePreviewController.h
@@ -0,0 +1,19 @@
+//
+//  ImagePreviewController.h
+//  ImagePreviewController
+//
+//  Created by Steven on 2017/4/26.
+//  Copyright © 2017年 cn.steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImagePreviewController.
+FOUNDATION_EXPORT double ImagePreviewControllerVersionNumber;
+
+//! Project version string for ImagePreviewController.
+FOUNDATION_EXPORT const unsigned char ImagePreviewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImagePreviewController/PublicHeader.h>
+
+
